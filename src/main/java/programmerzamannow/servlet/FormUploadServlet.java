d60f97b1b73657f7b6c210aba4466d83af28d1aa
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-04-10 20:30:11 +0700

    Upload File

---
+++ src/main/java/programmerzamannow/servlet/FormUploadServlet.java
@@ -0,0 +1,37 @@
+package programmerzamannow.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@WebServlet(urlPatterns = "/form-upload")
+@MultipartConfig
+public class FormUploadServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Path path = Path.of(FormUploadServlet.class.getResource("/html/form-upload.html").getPath());
+        String html = Files.readString(path);
+        resp.getWriter().println(html);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        Part profile = req.getPart("profile");
+
+        Path uploadLocation = Path.of("upload/" + UUID.randomUUID().toString() + profile.getSubmittedFileName());
+        Files.copy(profile.getInputStream(), uploadLocation);
+
+        resp.getWriter().println("Hello " + name + ", your profile saved in " + uploadLocation.toAbsolutePath());
+    }
+}
